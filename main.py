6519fd250b9812a9d5bd9b52017939f7eda0bae5
Jia Hao <71251813+jiah29@users.noreply.github.com>
2021-04-08 10:14:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+"""CSC111 Project: Picturing the Power of Policy in a Pandemic
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains the main functions for users to call and
+generate output, or to interact with the program as a whole.
+
+Do not edit any function statements in this module, unless otherwise
+specified.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of TAs and
+instructors of CSC111 at the University of Toronto St. George campus.
+All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+
+This file is Copyright (c) 2021 Jia Hao Choo & Komal Saini.
+"""
