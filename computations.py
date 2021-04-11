ce2bb87467eb699289a0d598d9529729874c7710
Jia Hao <71251813+jiah29@users.noreply.github.com>
2021-04-11 09:02:13 +0800

    Add files via upload

---
+++ computations.py
@@ -0,0 +1,20 @@
+"""CSC111 Project: Picturing the Power of Policy in a Pandemic
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains functions that does the computations
+on the WeightedGraph for simulations and graphs plotting.
+
+Do not edit anything in this module.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of TAs and
+instructors of CSC111 at the University of Toronto St. George campus.
+All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+
+This file is Copyright (c) 2021 Jia Hao Choo & Komal Saini.
+"""
